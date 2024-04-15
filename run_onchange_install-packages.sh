4fae9f51a6c39800d991cd41d806bad65f0e82fa
headheadhead <b08902062@csie.ntu.edu.tw>
2024-04-16 02:58:14 +0800

    add package for nvim

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 sudo apt update
-sudo apt install -y ripgrep bat zsh vim-gtk3
+sudo apt install -y ripgrep bat zsh vim-gtk3 libfuse2
 wget https://github.com/neovim/neovim/releases/download/v0.9.5/nvim.appimage && \
     chmod 555 nvim.appimage && sudo mv nvim.appimage /usr/bin/nvim
 sudo chsh -s /bin/zsh $USER
