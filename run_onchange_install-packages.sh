cc98202f134cb167608fea2378820ebb08349897
headheadhead <b08902062@csie.ntu.edu.tw>
2024-04-16 02:54:44 +0800

    install vim

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -1,7 +1,10 @@
 #! /bin/sh
 sudo apt update
-sudo apt install -y ripgrep bat zsh
-sudo update-alternatives --set /bin/vim.basic
+sudo apt install -y ripgrep bat zsh vim-gtk3
 wget https://github.com/neovim/neovim/releases/download/v0.9.5/nvim.appimage && \
     chmod 555 nvim.appimage && sudo mv nvim.appimage /usr/bin/nvim
 sudo chsh -s /bin/zsh $USER
+
+# vim config
+sudo update-alternatives --set /bin/vim.gtk3
+vim +BundleInstall +qall
