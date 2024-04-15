76099117ac243b8ca0a465f8955d4e7e718b43d5
headheadhead <b08902062@csie.ntu.edu.tw>
2024-04-16 02:46:08 +0800

    install zsh

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 sudo apt update
-sudo apt install ripgrep bat
+sudo apt install -y ripgrep bat zsh
 sudo update-alternatives --set /bin/vim.basic
 wget https://github.com/neovim/neovim/releases/download/v0.9.5/nvim.appimage && \
     chmod 555 nvim.appimage && sudo mv nvim.appimage /usr/bin/nvim
+sudo chsh -s /bin/zsh $USER
