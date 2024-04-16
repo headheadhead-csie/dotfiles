7fc01fe415ef2209ed85a9608cee803d6388c489
headheadhead <b08902062@csie.ntu.edu.tw>
2024-04-16 12:35:22 +0800

    change source list while using debian

--- run_once_before_install-basic-packages.sh
+++ run_once_before_install-basic-packages.sh
@@ -1,3 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 sudo apt update && sudo apt install -y wget ripgrep bat zsh vim-gtk3 \
-    libfuse2 curl
+    libfuse2 curl xterm exuberant-ctags git
+
+# chagne the apt source if using Debian
+if [[ -n $(cat /etc/*-release | grep -i "name.*debian") ]]; then
+    sed -i "s/deb.debian.org/debian.csie.ntu.edu.tw/g" /etc/apt/sources.list
+    sed -i "s/deb.debian.org/debian.csie.ntu.edu.tw/g" /etc/apt/mirrors/debian.list
+    sed -i "s/deb.debian.org/debian.csie.ntu.edu.tw/g" /etc/apt/mirrors/debian-security.list
+fi
