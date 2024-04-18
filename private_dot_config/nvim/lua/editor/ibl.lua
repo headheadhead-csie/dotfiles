3dd528b9a1282873cefc6472ccf4c61d32e505c2
headheadhead <b08902062@csie.ntu.edu.tw>
2024-04-18 14:58:42 +0800

    update ibl.lua

--- private_dot_config/nvim/lua/editor/ibl.lua
+++ private_dot_config/nvim/lua/editor/ibl.lua
@@ -1,6 +1,10 @@
+local char = require("ibl.config").default_config.indent.char
 require("ibl").setup({
     whitespace = {
         remove_blankline_trail = false,
+    },
+    indent = {
+        tab_char = char,
     },
     scope = {
         enabled = false,
