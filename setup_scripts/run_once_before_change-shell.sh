ad5fc0ff1d8e7650a2e9a6dbd42f7d53b6d2d876
headheadhead-csie <r12922072@csie.ntu.edu.tw>
2024-04-20 01:39:36 +0800

    fix all zsh related bugs

--- setup_scripts/run_once_before_change-shell.sh
+++ setup_scripts/run_once_before_change-shell.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+rm -rf ~/.oh-my-zsh/
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+rm -rf ~/.oh-my-zsh/plugins/
+sudo chsh -s $(which zsh) $(whoami)
