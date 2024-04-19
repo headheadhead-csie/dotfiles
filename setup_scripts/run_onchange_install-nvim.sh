59b77141d65ee23dd6405def835a71c140c398e8
headheadhead-csie <b08902062@csie.ntu.edu.tw>
2024-04-20 02:20:42 +0800

    fix nvim installation

--- setup_scripts/run_onchange_install-nvim.sh
+++ setup_scripts/run_onchange_install-nvim.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 wget https://github.com/neovim/neovim/releases/download/v0.9.5/nvim.appimage && \
-    chmod 555 nvim.appimage && mkdir ~/bin && mv nvim.appimage ~/bin/nvim
+    chmod 555 nvim.appimage
+mkdir ~/bin
+mv nvim.appimage ~/bin/nvim
