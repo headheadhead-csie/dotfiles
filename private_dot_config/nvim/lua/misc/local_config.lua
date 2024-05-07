49d094af4d3db92dc496941487ee0e7bfefe2337
headheadhead <b08902062@ntu.edu.tw>
2024-05-07 12:17:29 +0800

    nvim: silent local-config

--- private_dot_config/nvim/lua/misc/local_config.lua
+++ private_dot_config/nvim/lua/misc/local_config.lua
@@ -7,7 +7,7 @@ require('config-local').setup {
 
     autocommands_create = true, -- Create autocommands (VimEnter, DirectoryChanged)
     commands_create = true,     -- Create commands (ConfigLocalSource, ConfigLocalEdit, ConfigLocalTrust, ConfigLocalIgnore)
-    silent = false,             -- Disable plugin messages (Config loaded/ignored)
+    silent = true,             -- Disable plugin messages (Config loaded/ignored)
     lookup_parents = true,      -- Lookup config files in parent directories
 }
 
