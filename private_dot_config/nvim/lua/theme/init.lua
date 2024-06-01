21c23bac232922b73565d4f60ba4d1ded56802a3
headheadhead-csie <r12922072@csie.ntu.edu.tw>
2024-06-01 22:20:34 +0800

    nvim: set colortheme via global variable

--- private_dot_config/nvim/lua/theme/init.lua
+++ private_dot_config/nvim/lua/theme/init.lua
@@ -1,7 +1,8 @@
+current_theme = "kanagawa"
 require("theme.treesitter")
 require("theme.catppuccin")
 require("theme.lualine")
 require("theme.tundra")
 require("theme.kanagawa")
-vim.cmd.colorscheme "kanagawa"
+vim.cmd.colorscheme(current_theme)
 require("theme.bufferline")
