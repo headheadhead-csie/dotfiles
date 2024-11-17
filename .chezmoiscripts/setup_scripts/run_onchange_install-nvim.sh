11c64cb3ebd61f931d1ea54d36f7afb749da6404
headheadhead-csie <r12922072@csie.ntu.edu.tw>
2024-11-17 17:09:07 +0800

    script: download stable nvim

--- .chezmoiscripts/setup_scripts/run_onchange_install-nvim.sh
+++ .chezmoiscripts/setup_scripts/run_onchange_install-nvim.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://github.com/neovim/neovim/releases/download/v0.9.5/nvim.appimage && \
+wget https://github.com/neovim/neovim/releases/download/stable/nvim.appimage && \
     chmod 555 nvim.appimage
 mkdir ~/bin
 mv nvim.appimage ~/bin/nvim
