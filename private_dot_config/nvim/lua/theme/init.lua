29d67ccccf9fdcb93c5f5f339cb5ab3484c6c80c
headheadhead <b08902062@csie.ntu.edu.tw>
2024-12-13 16:32:32 +0800

    switch between ligth and dark theme
    For kitty, the environment variable `THEME` should be set before
    it is launched. That is, simply change .shrc won't work.

--- private_dot_config/nvim/lua/theme/init.lua
+++ private_dot_config/nvim/lua/theme/init.lua
@@ -1,4 +1,9 @@
-current_theme = "rose-pine"
+if vim.env.THEME == "light" then
+    current_theme = "rose-pine"
+else
+    current_theme = "catppuccin"
+    -- current_theme = "kanagawa"
+end
 require("theme.treesitter")
 require("theme.catppuccin")
 require("theme.lualine")
