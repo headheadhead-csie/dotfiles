931831e72304bb2f3ca5266cfdf532ed159e00dc
headheadhead <b08902062@csie.ntu.edu.tw>
2024-12-14 15:10:05 +0800

    nvim: adjust rose-pine style

--- private_dot_config/nvim/lua/theme/rosepine.lua
+++ private_dot_config/nvim/lua/theme/rosepine.lua
@@ -1,8 +1,8 @@
 require("rose-pine").setup({
     variant = "dawn", -- auto, main, moon, or dawn
     highlight_groups = {
-        ["@variable.parameter"] = { fg = "iris", bold = true, italic = false },
-        ["@parameter"] = { fg = "iris", bold = true, italic = false },
+        ["@variable.parameter"] = { fg = "iris", italic = false },
+        ["@parameter"] = { fg = "iris", italic = false },
 
         ["@variable"] = { italic = false },
         ["@variable.member"] = { fg = "subtle", italic = false },
@@ -11,5 +11,8 @@ require("rose-pine").setup({
         ["@function"] = { italic = true },
         ["@keyword.return"] = { bold = true, },
         ["@constant.macro"] = { fg = "pine" },
+
+        ["@constant"] = { fg = "love" },
+        ["Number"] = { fg = "love" },
     }
 })
