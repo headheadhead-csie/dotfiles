f17581e8fd9bdefbf6867a4c86ba10c70908250b
headheadhead <b08902062@csie.ntu.edu.tw>
2024-12-14 14:19:47 +0800

    add p10k light theme

--- private_dot_config/nvim/lua/theme/rosepine.lua
+++ private_dot_config/nvim/lua/theme/rosepine.lua
@@ -1,3 +1,8 @@
 require("rose-pine").setup({
     variant = "dawn", -- auto, main, moon, or dawn
+    highlight_groups = {
+        ["@variable.parameter"] = { fg = "love", bold = true },
+        ["@parameter"] = { fg = "love", bold = true, },
+        ["@keyword.return"] = { bold = true, },
+    }
 })
