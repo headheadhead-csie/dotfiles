6ea7dd735dd61252b389c9d98da601f91ee649de
headheadhead <b08902062@csie.ntu.edu.tw>
2025-03-06 11:18:25 +0800

    nvim: update the link of nvim.appimage

--- .chezmoiscripts/setup_scripts/run_onchange_install-nvim.sh
+++ .chezmoiscripts/setup_scripts/run_onchange_install-nvim.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://github.com/neovim/neovim/releases/download/stable/nvim.appimage && \
+wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux-x86_64.appimage && \
     chmod 555 nvim.appimage
 mkdir ~/bin
 mv nvim.appimage ~/bin/nvim
