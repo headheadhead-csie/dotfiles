3a18911dfec732b4001c5ff6900fe33252446046
headheadhead-csie <r12922072@csie.ntu.edu.tw>
2025-03-31 17:48:30 +0800

    fix install scripts

--- .chezmoiscripts/setup_scripts/run_onchange_install-nvim.sh
+++ .chezmoiscripts/setup_scripts/run_onchange_install-nvim.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux-x86_64.appimage && \
-    chmod 555 nvim.appimage
+    chmod 555 nvim-linux-x86_64.appimage
 mkdir ~/bin
-mv nvim.appimage ~/bin/nvim
+mv nvim-linux-x86_64.appimage ~/bin/nvim
