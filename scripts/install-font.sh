e04c47dd25f85f871ec6be011cb4cf5a5ba125a1
headheadhead <b08902062@csie.ntu.edu.tw>
2025-05-31 13:56:15 +0800

    install-font: update nerd font

--- scripts/install-font.sh
+++ scripts/install-font.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/JetBrainsMono.zip
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/JetBrainsMono.zip
 unzip JetBrainsMono.zip -d /usr/share/fonts/truetype/JetBrainsMono
 rm JetBrainsMono.zip
