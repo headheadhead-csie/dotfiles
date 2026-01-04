049dcab34b94949968fd1910980c96d18860f12c
headheadhead-csie <r12922072@csie.ntu.edu.tw>
2026-01-04 22:15:17 +0800

    update run_once_before for different release

--- scripts/run_once_before/install-basic-packages.sh
+++ scripts/run_once_before/install-basic-packages.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-sudo apt update && sudo apt install -y wget ripgrep bat zsh vim-gtk3 \
-    libfuse2 curl xterm git openssh-client openssh-server tig \
-    iproute2 iptables bridge-utils tcpdump python3-venv\
+# This script should be called by run_once_before_script.sh
+
+package_list="wget ripgrep bat zsh vim-gtk3 libfuse2 curl xterm
+    git openssh-client openssh-server tig iproute2 iptables
+    bridge-utils tcpdump python3-venv"
+
+update_package
+for package in ${package_list}; do
+    install_package $package
+done
 
 # change the apt source if using Debian
 if [[ -n $(cat /etc/*-release | grep -i "name.*debian") ]]; then
