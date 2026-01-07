3752330f431a519f51333fbc7167748e88ca0d05
headheadhead <b08902062@csie.ntu.edu.tw>
2026-01-07 15:49:30 +0800

    nvim: add support for treesitter-textobjects movement

--- private_dot_config/nvim/lua/editor/ts_to.lua
+++ private_dot_config/nvim/lua/editor/ts_to.lua
@@ -1,44 +1,37 @@
-return function()
-     require('nvim-treesitter.configs').setup({
-         textobjects = {
-             move = {
-                 enable = true,
-                 set_jumps = true, -- whether to set jumps in the jumplist
-                 goto_next_start = {
-                     ["]]"] = "@function.outer",
-                     ["]c"] = { query = "@class.outer", desc = "Next class start" },
-                     --
-                     -- You can use regex matching (i.e. lua pattern) and/or pass a list in a "query" key to group multiple queires.
-                     -- ["]o"] = "@loop.*",
-                     -- ["]o"] = { query = { "@loop.inner", "@loop.outer" } }
-                     --
-                     -- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
-                     -- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
-                     -- ["]s"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
-                     -- ["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
-                 },
-                 goto_next_end = {
-                     ["]["] = "@function.outer",
-                     ["]C"] = "@class.outer",
-                 },
-                 goto_previous_start = {
-                     ["[["] = "@function.outer",
-                     ["[c"] = "@class.outer",
-                 },
-                 goto_previous_end = {
-                     ["[]"] = "@function.outer",
-                     ["[C"] = "@class.outer",
-                 },
-                 -- Below will go to either the start or the end, whichever is closer.
-                 -- Use if you want more granular movements
-                 -- Make it even more gradual by adding multiple queries and regex.
-                 -- goto_next = {
-                 --     ["]d"] = "@conditional.outer",
-                 -- },
-                 -- goto_previous = {
-                 --     ["[d"] = "@conditional.outer",
-                 -- }
-             },
-         },
-    })
-end
+-- configuration
+require("nvim-treesitter-textobjects").setup {
+  move = {
+    -- whether to set jumps in the jumplist
+    set_jumps = true,
+  },
+}
+
+-- keymaps
+-- You can use the capture groups defined in `textobjects.scm`
+vim.keymap.set({ "n", "x", "o" }, "]]", function()
+  require("nvim-treesitter-textobjects.move").goto_next_start("@function.outer", "textobjects")
+end)
+vim.keymap.set({ "n", "x", "o" }, "]c", function()
+  require("nvim-treesitter-textobjects.move").goto_next_start("@class.outer", "textobjects")
+end)
+
+vim.keymap.set({ "n", "x", "o" }, "][", function()
+  require("nvim-treesitter-textobjects.move").goto_next_end("@function.outer", "textobjects")
+end)
+vim.keymap.set({ "n", "x", "o" }, "]C", function()
+  require("nvim-treesitter-textobjects.move").goto_next_end("@class.outer", "textobjects")
+end)
+
+vim.keymap.set({ "n", "x", "o" }, "[[", function()
+  require("nvim-treesitter-textobjects.move").goto_previous_start("@function.outer", "textobjects")
+end)
+vim.keymap.set({ "n", "x", "o" }, "[c", function()
+  require("nvim-treesitter-textobjects.move").goto_previous_start("@class.outer", "textobjects")
+end)
+
+vim.keymap.set({ "n", "x", "o" }, "[]", function()
+  require("nvim-treesitter-textobjects.move").goto_previous_end("@function.outer", "textobjects")
+end)
+vim.keymap.set({ "n", "x", "o" }, "[C", function()
+  require("nvim-treesitter-textobjects.move").goto_previous_end("@class.outer", "textobjects")
+end)
