cb9c507b2e746d78b76e844561c59388eefd6955
headheadhead <b08902062@csie.ntu.edu.tw>
2026-01-16 16:08:11 +0800

    nvim: add lsp outgoing calls and incoming calls

--- private_dot_config/nvim/lua/lsp/lsp-zero.lua
+++ private_dot_config/nvim/lua/lsp/lsp-zero.lua
@@ -76,6 +76,16 @@ function use_trouble(buffer)
         '<cmd>Trouble lsp_implementations<CR>',
         { silent = true, noremap = true, buffer = buffer }
     )
+    vim.keymap.set(
+        'n', 'gI',
+        '<cmd>Trouble lsp_incoming_calls<CR>',
+        { silent = true, noremap = true, buffer = buffer }
+    )
+    vim.keymap.set(
+        'n', 'gO',
+        '<cmd>Trouble lsp_outgoing_calls<CR>',
+        { silent = true, noremap = true, buffer = buffer }
+    )
     vim.cmd("hi SagaBeacon guibg=#89dceb")
 end
 
@@ -109,7 +119,10 @@ vim.api.nvim_create_autocmd('LspAttach', {
         vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
         vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
         vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+        vim.keymap.set('n', 'gI', '<cmd>lua vim.lsp.buf.incoming_calls()<cr>', opts)
         vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+        vim.keymap.set('n', 'gI', '<cmd>lua vim.lsp.buf.incoming_calls()<cr>', opts)
+        vim.keymap.set('n', 'gO', '<cmd>lua vim.lsp.buf.outgoing_calls()<cr>', opts)
         vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
         vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
         vim.keymap.set('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
